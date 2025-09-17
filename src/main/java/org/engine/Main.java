236694de364cc45f80f55e471cd19240667b8d40
tbreese3 <tylerbreese2001@gmail.com>
2025-09-17 19:23:12 -0400

    Fix

--- src/main/java/org/engine/Main.java
+++ src/main/java/org/engine/Main.java
@@ -23,6 +23,10 @@ public class Main {
     }
 
 	public static void main(String[] args) throws Exception {
+		if (args != null && args.length > 0 && args[0].equalsIgnoreCase("pgo")) {
+			UCI.runPgoMovetime(15000);
+			return;
+		}
 		if (args != null && args.length > 0 && args[0].equalsIgnoreCase("bench")) {
 			int depth = 4;
 			if (args.length > 1) try { depth = Integer.parseInt(args[1]); } catch (Exception ignored) {}
