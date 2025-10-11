4f3cdf88afd9c4b4c95587beca3a9a17f1a62fbc
tbreese3 <162940756+tbreese3@users.noreply.github.com>
2025-10-11 02:41:25 -1000

    SPSA Tune (#54)
    * Fix
    
    * Fix

--- src/main/java/org/engine/SPSA.java
+++ src/main/java/org/engine/SPSA.java
@@ -1,28 +1,28 @@
 package org.engine;
 
-/*
-NMPBase, int, 2.0, 0.0, 10.0, 0.5, 0.005
-NMPDepthScale100, int, 23.0, 0.0, 200.0, 2.0, 0.005
-NMPEvalMargin, int, 241.0, 1.0, 4000.0, 50.0, 0.003
+/* (300 Iterations per param)
+NMPBase, int, 3.0, 0.0, 10.0, 0.5, 0.005
+NMPDepthScale100, int, 21.0, 0.0, 200.0, 2.0, 0.005
+NMPEvalMargin, int, 242.0, 1.0, 4000.0, 50.0, 0.003
 NMPEvalMax, int, 4.0, 0.0, 10.0, 0.5, 0.005
 LMRBase100, int, 77.0, 0.0, 300.0, 5.0, 0.003
-LMRDivisor100, int, 216.0, 1.0, 1000.0, 10.0, 0.003
+LMRDivisor100, int, 213.0, 1.0, 1000.0, 10.0, 0.003
 ReverseFUTMaxDepth, int, 3.0, 0.0, 8.0, 0.3, 0.00333333
-ReverseFUTMarginPerDepth, int, 111.0, 0.0, 1024.0, 16.0, 0.003
+ReverseFUTMarginPerDepth, int, 96.0, 0.0, 1024.0, 16.0, 0.003
 FUTMaxDepth, int, 2.0, 0.0, 8.0, 0.3, 0.00333333
-FUTMarginPerDepth, int, 110.0, 0.0, 1024.0, 16.0, 0.003
-QSeeMargin, int, -7.0, -1024.0, 1024.0, 8.0, 0.003
+FUTMarginPerDepth, int, 109.0, 0.0, 1024.0, 16.0, 0.003
+QSeeMargin, int, 0.0, -1024.0, 1024.0, 8.0, 0.003
 LMPMaxDepth, int, 3.0, 0.0, 8.0, 0.3, 0.00333333
 LMPBaseThreshold, int, 4.0, 0.0, 64.0, 1.0, 0.00333333
 LMPPerDepth, int, 1.0, 0.0, 16.0, 1.0, 0.00333333
-LMPMarginPerDepth, int, 129.0, 0.0, 1024.0, 16.0, 0.003
+LMPMarginPerDepth, int, 139.0, 0.0, 1024.0, 16.0, 0.003
 IIRMinPVDepth, int, 2.0, 0.0, 16.0, 1.0, 0.005
-IIRMinCutDepth, int, 3.0, 0.0, 16.0, 1.0, 0.005
-SingularMinDepth, int, 7.0, 0.0, 32.0, 1.0, 0.005
-SingularMarginPerDepth, int, 2.0, 0.0, 8.0, 1.0, 0.005
-TMHeuristicsMinDepth, int, 6.0, 0.0, 32.0, 1.0, 0.005
-TMMaxExtensionFactor100, int, 350.0, 100.0, 1000.0, 10.0, 0.003
-TMInstabilityScoreWeight10000, int, 70.0, 0.0, 1000.0, 5.0, 0.003
+IIRMinCutDepth, int, 4.0, 0.0, 16.0, 1.0, 0.005
+SingularMinDepth, int, 6.0, 0.0, 32.0, 1.0, 0.005
+SingularMarginPerDepth, int, 0.0, 0.0, 8.0, 1.0, 0.005
+TMHeuristicsMinDepth, int, 5.0, 0.0, 32.0, 1.0, 0.005
+TMMaxExtensionFactor100, int, 356.0, 100.0, 1000.0, 10.0, 0.003
+TMInstabilityScoreWeight10000, int, 69.0, 0.0, 1000.0, 5.0, 0.003
 
 Notes:
 - LMRBase100 -> lmrBase = value / 100.0
@@ -33,27 +33,27 @@ Notes:
 
 public final class SPSA {    
     public double lmrBase = 0.77;
-    public double lmrDivisor = 2.16;
+    public double lmrDivisor = 2.13;
     public int reverseFutilityMaxDepth = 3;
-    public int reverseFutilityMarginPerDepth = 111;
+    public int reverseFutilityMarginPerDepth = 96;
     public int futilityMaxDepth = 2;
-    public int futilityMarginPerDepth = 110;
-    public int lmpMarginPerDepth = 129;
-    public int qseeMargin = -7;
-    public int nmpBase = 2;
-    public double nmpDepthScale = 0.23;
-    public int nmpEvalMargin = 241;
+    public int futilityMarginPerDepth = 109;
+    public int lmpMarginPerDepth = 139;
+    public int qseeMargin = 0;
+    public int nmpBase = 3;
+    public double nmpDepthScale = 0.21;
+    public int nmpEvalMargin = 242;
     public int nmpEvalMax = 4;
     public int lmpMaxDepth = 3;
     public int lmpBaseThreshold = 4;
     public int lmpPerDepth = 1;
     public int iirMinPVDepth = 2;
-    public int iirMinCutDepth = 3;
-    public int singularMinDepth = 7;
-    public int singularMarginPerDepth = 2;
-    public int tmHeuristicsMinDepth = 6;
-    public double tmMaxExtensionFactor = 3.5;
-    public double tmInstabilityScoreWeight = 0.007;
+    public int iirMinCutDepth = 4;
+    public int singularMinDepth = 6;
+    public int singularMarginPerDepth = 0;
+    public int tmHeuristicsMinDepth = 5;
+    public double tmMaxExtensionFactor = 3.56;
+    public double tmInstabilityScoreWeight = 0.0069;
 
     public void setByName(String name, int value) {
         if (name == null) return;
